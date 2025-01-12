a126805f03058bbf3d6dfce5ed6fc84b30c21344
RafaelIagherCosta <fael.iagher28082007@gmail.com>
2025-01-12 07:50:24 -0300

    chore: add netlify

--- galeria.js
+++ galeria.js
@@ -1,27 +1,30 @@
-$(document).ready(function(){
- $('header button').click(function(){
-    $('form').slideDown()
- })
+$(document).ready(function () {
+    $('header button').click(function () {
+        $('form').slideDown()
+    })
 
- $('#botao-cancelar').click(function(){
-    $('form').slideUp()
- })
+    $('#botao-cancelar').click(function () {
+        $('form').slideUp()
+    })
 
-$('form').on('submit', function(e){
-    e.preventDefault()
-    const enderecoDaNovaImagem = $('#endereco-imagem-nova').val()
-    const novoItem = $('<li> </li>')
-    $(`<img src="${enderecoDaNovaImagem}"</img>`).appendTo(novoItem);
-    $(`
-        <div class="overlay-imagem-link">
-            <a href="enderecoDaNovaImagem" title="Ver imagem em tamanho real" target="_black">
-                Ver imagem em tamanho real
-                </a>
-        </div>
-        `).appendTo(novoItem);
+    $('form').on('submit', function (e) {
+        e.preventDefault()
+        const enderecoDaNovaImagem = $('#endereco-imagem-nova').val()
+        const novoItem = $('<li style="display:none">  </li>')
+        $(`<img src="${enderecoDaNovaImagem}"</img>`).appendTo(novoItem);
+        $(`
+            <div class="overlay-imagem-link">
+                <a href="enderecoDaNovaImagem" title="Ver imagem em tamanho real" target="_black">
+                    Ver imagem em tamanho real
+                    </a>
+            </div>
+            `).appendTo(novoItem);
         $(novoItem).appendTo('ul');
-        $('#endereco-imagem-nova').val('')
+        $(novoItem).fadeIn();
+        $('#endereco-imagem-nova').val('');
 
 
+    })
 })
-})
+
+  
